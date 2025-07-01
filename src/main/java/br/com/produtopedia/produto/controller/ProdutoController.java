add122deb76de55780e6492a362e60d767f146cc
gustavosrosa <g.dsantosrosa@gmail.com>
2025-07-01 12:27:18 -0300

    PROD: FEAT: Anotacao req body

--- src/main/java/br/com/produtopedia/produto/controller/ProdutoController.java
+++ src/main/java/br/com/produtopedia/produto/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package br.com.produtopedia.produto.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,8 @@ import br.com.produtopedia.produto.model.Produto;
 @RequestMapping("api")
 public class ProdutoController {
 	
-	public ResponseEntity<DefaultResponseModel> salvar(Produto produto) {
+	@PostMapping("produto")
+	public ResponseEntity<DefaultResponseModel> salvar(@RequestBody Produto produto) {
 		return new ResponseEntity<DefaultResponseModel>(new DefaultResponseModel(HttpStatus.CREATED.value(), "Criado com sucesso"), HttpStatus.CREATED);
 	}
 
--- src/main/java/br/com/produtopedia/produto/model/Produto.java
+++ src/main/java/br/com/produtopedia/produto/model/Produto.java
@@ -2,6 +2,15 @@ package br.com.produtopedia.produto.model;
 
 import java.math.BigDecimal;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
 	
 	private Long id;
